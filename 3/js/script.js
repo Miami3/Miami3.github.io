cbb2bf4dc9c18c280d1d675f0f1ac1b3f6f1655e
Miami3 <olegkalysh25@gmail.com>
2016-07-29 05:23:00 +0300

    3

---
+++ 3/js/script.js
@@ -0,0 +1,10 @@
+// Mobile Menu Toogle
+$(document).ready(
+    function(){
+        $('.mobile-menu').click(
+            function(){
+                $('.menu-list').slideToggle(400);
+            }
+        );
+    }
+);
